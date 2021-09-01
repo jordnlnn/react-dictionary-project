9268b8be7414381fbf5481c34b18f2254ec8694e
jordnlnn <80535966+jordnlnn@users.noreply.github.com>
2021-08-31 21:43:42 -0700

    integrated dictionary API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,19 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 export default function Dictionary() {
 let [keyword, setKeyword] = useState(null);
 
+function handleResponse(response) {
+    console.log(response.data[0]);
+}
+
 function search(event) {
     event.preventDefault();
-    alert(`Searching for the definition of ${keyword}`);
+    
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    console.log(apiUrl);
+    axios.get(apiUrl).then(handleResponse); 
 }
 
 function handleKeywordChange(event) {
