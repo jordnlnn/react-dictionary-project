5fe29a65b0c8bee62e0f2681debfd370c16bed09
jordnlnn <80535966+jordnlnn@users.noreply.github.com>
2021-09-05 17:26:29 -0700

    added synonyms component

---
+++ src/Synonyms.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function Synonyms(props) {
+    if (props.synonyms) {
+        return (
+            <div className="Synonyms">
+            <ul>
+            {props.synonyms.map(function (synonym, index) {
+                return (
+                    <li key={index}>
+                    {synonym}
+                    </li>
+                )
+            })}
+            </ul>
+            </div>
+        );
+    } else {
+        return null;
+    }
+}
