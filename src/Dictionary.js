ad3fcebc76512a5beda8b429eafaa3e8c549bb4d
jordnlnn <80535966+jordnlnn@users.noreply.github.com>
2021-09-06 15:11:33 -0700

    added more css styling and integrated Pexels API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,14 +1,21 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 
 export default function Dictionary(props) {
 let [keyword, setKeyword] = useState(props.defaultKeyword);
 let [results, setResults] = useState(null);
 let [loaded, setLoaded] = useState(false);
+let [photos, setPhotos] = useState(null);
 
-function handleResponse(response) {
+function handleDictionaryResponse(response) {
     setResults(response.data[0]);
+}
+
+function handlePexelsResponse(response) {
+    //console.log(response.data);
+    setPhotos(response.data.photos);
 }
 
 function handleSubmit(event) {
@@ -19,7 +26,11 @@ function handleSubmit(event) {
 function search () {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     //console.log(apiUrl);
-    axios.get(apiUrl).then(handleResponse); 
+    axios.get(apiUrl).then(handleDictionaryResponse); 
+
+    let pexelsApiKey = "563492ad6f9170000100000194d06b4a67e842099fe90bdccbb11d89";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=6`;
+    axios.get(pexelsApiUrl, { headers: {"Authorization" : `Bearer ${pexelsApiKey}` }, }).then(handlePexelsResponse);
 }
 
 function handleKeywordChange(event) {
@@ -41,6 +52,7 @@ if (loaded) {
         </form>
         </section>
         <Results results={results} />
+        <Photos photos={photos} />
         </div>
     )
 
